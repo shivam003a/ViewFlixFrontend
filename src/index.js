da0b1418b14610e4000c3a0549398f42d1a4689c
Shivam Singh <shivam003a@gmail.com>
2024-01-31 02:18:18 +0530

    fetch api correct

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
 import {store} from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter>
+	<HashRouter>
 		<Provider store={store}>
 			<App />
 			<Toaster position='bottom-right' />
 		</Provider>
-	</BrowserRouter>
+	</HashRouter>
 );
