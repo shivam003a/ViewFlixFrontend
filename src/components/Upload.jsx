f6247a3d959d8a1731a95f59ec3875d123f857bd
Shivam Singh <shivam003a@gmail.com>
2024-03-04 13:06:06 +0530

    compression commit: added compression techniques to the thumbnail

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -3,11 +3,12 @@ import './css/Upload.css'
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 import app from '../firebase/firebase'
 import toast from 'react-hot-toast'
-import {useSelector} from 'react-redux'
-import {NavLink, useNavigate} from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { NavLink, useNavigate } from 'react-router-dom'
+import Compressor from 'compressorjs'
 
 const Upload = () => {
-    const navigate =  useNavigate()
+    const navigate = useNavigate()
 
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
@@ -19,39 +20,48 @@ const Upload = () => {
     const [imgUrl, setImgUrl] = useState('')
     const [videoUrl, setVideoUrl] = useState('')
 
-    const {logged} = useSelector((state)=>{
+    const { logged } = useSelector((state) => {
         return state.user
     })
 
     useEffect(() => {
         const upload = async () => {
-            const storage = getStorage(app)
-            const fileName = new Date().getTime() + image.name
-            const storageRef = ref(storage, fileName);
+            new Compressor(image, {
+                quality: 0.4,
+                success(result) {
+                    const storage = getStorage(app)
+                    const fileName = new Date().getTime() + result.name
+                    const storageRef = ref(storage, fileName);
 
-            const uploadTask = uploadBytesResumable(storageRef, image);
-            uploadTask.on('state_changed',
-                (snapshot) => {
-                    const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
-                    setImgProgress(progress)
-                    switch (snapshot.state) {
-                        case "paused":
-                            console.log('paused')
-                            break;
-                        case 'running':
-                            console.log('upload running')
-                            break
-                    }
+                    const uploadTask = uploadBytesResumable(storageRef, result);
+                    uploadTask.on('state_changed',
+                        (snapshot) => {
+                            const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+                            setImgProgress(progress)
+                            switch (snapshot.state) {
+                                case "paused":
+                                    console.log('paused')
+                                    break;
+                                case 'running':
+                                    console.log('upload running')
+                                    break
+                            }
+                        },
+                        (error) => {
+                            console.log(error)
+                        },
+                        () => {
+                            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                                setImgUrl(downloadURL)
+                            })
+                        }
+                    )
                 },
-                (error) => {
-                    console.log(error)
-                },
-                () => {
-                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                        setImgUrl(downloadURL)
-                    })
+                error(err) {
+                    console.log(err)
                 }
-            )
+            })
+
         }
         image && upload()
     }, [image])
@@ -116,26 +126,26 @@ const Upload = () => {
     return (
         <div className="upload-container">
             {
-                logged?(
+                logged ? (
                     <div className='upload-card'>
-                <p className='upload'>Upload</p>
-                <div className='content-input'>
-                    <span>video</span>
-                    <input type='file' name='video' accept='video/*' onChange={(e) => { setVideo(e.target.files[0]) }} />
-                    <div className='progress'>{videoProgress}%</div>
-                </div>
-                <div className='content-input'>
-                    <span>image</span>
-                    <input type='file' name='image' accept='image/*' onChange={(e) => { setImage(e.target.files[0]) }} />
-                    <div className='progress'>{imgProgress}%</div>
-                </div>
-                <input type='text' name='title' placeholder='title' onChange={(e) => { setTitle(e.target.value) }} />
-                <textarea name='description' rows={7} placeholder='description' onChange={(e) => { setDescription(e.target.value) }}></textarea>
-                <input type='text' name='tags' placeholder='tags separated by comma (,)' onChange={(e) => { setTags(e.target.value.split(',')) }} />
-                <button className='upload-button' onClick={handleUpload}>Upload</button>
-            </div>
-                ):(
-                    <NavLink to="/signin" className="button" style={{textDecoration: "none"}}>SignIn</NavLink>
+                        <p className='upload'>Upload</p>
+                        <div className='content-input'>
+                            <span>video</span>
+                            <input type='file' name='video' accept='video/*' onChange={(e) => { setVideo(e.target.files[0]) }} />
+                            <div className='progress'>{videoProgress}%</div>
+                        </div>
+                        <div className='content-input'>
+                            <span>image</span>
+                            <input type='file' name='image' accept='image/*' onChange={(e) => { setImage(e.target.files[0]) }} />
+                            <div className='progress'>{imgProgress}%</div>
+                        </div>
+                        <input type='text' name='title' placeholder='title' onChange={(e) => { setTitle(e.target.value) }} />
+                        <textarea name='description' rows={7} placeholder='description' onChange={(e) => { setDescription(e.target.value) }}></textarea>
+                        <input type='text' name='tags' placeholder='tags separated by comma (,)' onChange={(e) => { setTags(e.target.value.split(',')) }} />
+                        <button className='upload-button' onClick={handleUpload}>Upload</button>
+                    </div>
+                ) : (
+                    <NavLink to="/signin" className="button" style={{ textDecoration: "none" }}>SignIn</NavLink>
                 )
             }
         </div>
